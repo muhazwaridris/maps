cadec5a83c35306ce4c0ab4ac1935eec10923944
Muh Azwar Idris <muhazwaridris@ymail.com>
2017-07-06 12:00:03 +0800

    Add files via upload

---
+++ mapmarkers.php
@@ -0,0 +1,54 @@
+<?php
+
+require("db.php");
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+// 1. Create a database connection
+$connection = mysqli_connect('localhost', $username, $password);
+if (!$connection) {
+    die("Database connection failed: " . mysqli_error());
+}
+
+// 2. Select a database to use 
+$db_select = mysqli_select_db($connection, 'maps');
+if (!$db_select) {
+    die("Database selection failed: " . mysqli_error());
+}
+
+// Select all the rows in the markers table
+$query = "SELECT * FROM markers WHERE 1";
+$result = mysqli_query($connection, $query);
+if (!$result) {
+  die('Invalid query: ' . mysqli_error());
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysqli_fetch_assoc($result)){
+  // Add to XML document node
+  echo '<marker ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'address="' . parseToXML($row['address']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'type="' . $row['type'] . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
+?>
